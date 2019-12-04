21a447d8372eb1ba72d5c170e6c5e1977c229f8d
Vasyl Shpak <vasyl.shpak4@gmail.com>
2019-12-04 22:16:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Ball from './Ball'
+import Lottery from './Lottery';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title='mini daily' maxNum={10} maxBalls={4} />
+      </div>
+    );
+  }
+}
+
+
+export default App;
