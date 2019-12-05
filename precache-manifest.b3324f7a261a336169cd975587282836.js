1d08274d65567fc7512d11ff02ca4ecdd55ec6b8
Vasyl Shpak <vasyl.shpak4@gmail.com>
2019-12-05 22:48:47 +0100

    Updates

---
+++ precache-manifest.b3324f7a261a336169cd975587282836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c9b5483163b3188e1a413137ac0fcf",
+    "url": "/lotto-balls/index.html"
+  },
+  {
+    "revision": "44e1d0cb7157ae1a3d27",
+    "url": "/lotto-balls/static/css/main.00ef039b.chunk.css"
+  },
+  {
+    "revision": "172f8fa2930e77e11a8c",
+    "url": "/lotto-balls/static/js/2.82f37f90.chunk.js"
+  },
+  {
+    "revision": "44e1d0cb7157ae1a3d27",
+    "url": "/lotto-balls/static/js/main.7f90e4c1.chunk.js"
+  },
+  {
+    "revision": "a45c9907403df1440853",
+    "url": "/lotto-balls/static/js/runtime-main.a84aa64c.js"
+  }
+]);
